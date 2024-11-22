5ccb8e851eee17b8d02041ad8522442f3de8d095
Saikat <saikatsaha.rph@gmail.com>
2024-11-22 14:13:18 +0530

    fixed error

--- server/server.js
+++ server/server.js
@@ -14,9 +14,17 @@ app.use(express.json());
 app.use(cors());
 
 
+
+
 //API routes
 app.get('/', (req, res) => {
     res.send("API working")
 });
 
-app.listen(PORT, () => console.log("Server running on port", +PORT))
+// app.use((req, res, next) => {
+//     res.status(404).json({ error: "Route not found" });
+// });
+
+
+
+app.listen(PORT, () => console.log("Server running on port", PORT))
