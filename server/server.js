6c18338a6e42d8c49ca317eec4f1db3796ba083a
Saikat <saikatsaha.rph@gmail.com>
2024-11-25 07:36:14 +0530

    added webhook

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/mongodb.js';
+import userRouter from './routes/userRoute.js';
 
 
 //App Config
@@ -21,9 +22,11 @@ app.get('/', (req, res) => {
     res.send("API working")
 });
 
-app.use((req, res, next) => {
-    res.status(404).json({ error: "Route not found" });
-});
+app.use("/api/user", userRouter);
+
+// app.use((req, res, next) => {
+//     res.status(404).json({ error: "Route not found" });
+// });
 
 
 
