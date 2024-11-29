447647896621133d4886725d7434bc21ddd5de93
Saikat <saikatsaha.rph@gmail.com>
2024-11-29 14:21:02 +0530

    nothing

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import userRouter from './routes/userRoute.js';
 
 
 //App Config
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 connectDB()
 
@@ -15,19 +15,12 @@ app.use(express.json());
 app.use(cors());
 
 
-
-
 //API routes
 app.get('/', (req, res) => {
     res.send("API working")
 });
 
 app.use("/api/user", userRouter);
-
-// app.use((req, res, next) => {
-//     res.status(404).json({ error: "Route not found" });
-// });
-
 
 
 app.listen(PORT, () => console.log("Server running on port", PORT))
