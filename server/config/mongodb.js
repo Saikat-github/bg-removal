8679fab473f064cdcbe5a310aadef73d0adcaaa8
Saikat <saikatsaha.rph@gmail.com>
2024-12-12 10:57:46 +0530

    changed database configaration

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => {
-        console.log("Database Connected");
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
+    // mongoose.connection.on('connected', () => {
+    //     console.log("Database Connected");
+    // })
+    // await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
+
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Database connected"))
+    .catch((error) => console.log("Database error : ", error.message))
 }
 
 
